3a42467cef80bef3cccdef080ef809a0925e81a3
anhnd1223 <130715369+anhnd1223@users.noreply.github.com>
2023-04-28 09:11:43 +0700

    Add files via upload

---
+++ lab02/Cart.java
@@ -0,0 +1,16 @@
+
+public class Cart {
+	public static final int MAX_NUMBER_ORDERED = 20;
+	private DVD itemsOrdered[] = new DVD[MAX_NUMBER_ORDERED];
+	int qtyOrdered = 0;
+	void addDVD(DVD disc) {
+		
+	}
+	void removeDVD(DVD disc) {
+		
+	}
+	float totalCost() {
+		float i=0;
+		return i;
+	}
+}
