826d9c28b17b3b5555fe94657a1b77c01c471e83
anhnd1223 <130715369+anhnd1223@users.noreply.github.com>
2023-05-15 22:01:02 +0700

    Add files via upload

---
+++ Other Projects/ConcatenationInLoops.java
@@ -0,0 +1,24 @@
+package garbage;
+import java.util.*;
+
+public class ConcatenationInLoops {
+
+	public static void main(String[] args) {
+		Random r = new Random(123);
+		long start = System.currentTimeMillis();
+		String s ="";
+		for(int i = 0;i<192;i++)
+			s += r.nextInt(2);
+		System.out.println(System.currentTimeMillis());
+		
+		r = new Random(123);
+		start = System.currentTimeMillis();
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0;i<192;i++)
+			sb.append(r.nextInt(2));
+		s = sb.toString();
+		System.out.println(System.currentTimeMillis());
+		
+	}
+
+}
