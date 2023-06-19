3190c766f2856d5c10e81e94d3ab74d5e8dd4082
anhnd1223 <130715369+anhnd1223@users.noreply.github.com>
2023-06-19 23:49:47 +0700

    Add files via upload

---
+++ GUIProject/src/swing/SwingAccumulator.java
@@ -0,0 +1,45 @@
+package swing;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class SwingAccumulator extends JFrame {
+	private JTextField tfInput;
+	private JTextField tfOutput;
+	private int sum = 0;
+	 
+	public SwingAccumulator() {
+		Container cp = getContentPane();
+		cp.setLayout(new GridLayout(2,2));
+		
+		cp.add(new JLabel("Enter an Interger: "));
+		 
+		tfInput = new JTextField(10);
+		cp.add(tfInput);
+		tfInput.addActionListener(new TFInputListener());
+		
+		cp.add(new JLabel("The Accumalated Sum is: "));
+		
+		tfOutput = new JTextField(10);
+		tfOutput.setEditable(false);
+		cp.add(tfOutput);
+		
+		setTitle("Swing Accumulator");
+		setSize(350,120);
+		setVisible(true);
+	}
+	 
+	public static void main(String[] args) {
+		new SwingAccumulator();
+	}
+	 
+	private class TFInputListener implements ActionListener{
+		public void actionPerformed(ActionEvent evt) {
+			int numberIn = Integer.parseInt(tfInput.getText());
+			sum += numberIn;
+			tfInput.setText("");
+			tfOutput.setText(sum + "");
+		}
+	}
+	
+}
